8dd7be3bf0b55b11a11b51847b8dc16e87355e2b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-27 03:20:28 +0530

    adding AngleLineArc class

--- anglelinearcsview/src/main/java/com/anwesh/uiprojects/anglelinearcsview/AngleLineArcsView.kt
+++ anglelinearcsview/src/main/java/com/anwesh/uiprojects/anglelinearcsview/AngleLineArcsView.kt
@@ -179,7 +179,31 @@ class AngleLineArcsView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class AngleLineArc(var i : Int) {
+
+        private val root : ALANode = ALANode(0)
+        private var curr : ALANode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
