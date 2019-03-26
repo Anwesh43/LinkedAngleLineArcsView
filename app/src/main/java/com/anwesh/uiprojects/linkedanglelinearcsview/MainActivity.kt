73706437199c47ff62f10e849b2023c5b1a1d080
Anwesh43 <anweshthecool0@gmail.com>
2019-03-27 03:27:19 +0530

    adding app in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedanglelinearcsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedanglelinearcsview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedanglelinearcsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.anglelinearcsview.AngleLineArcsView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AngleLineArcsView.create(this)
     }
 }
